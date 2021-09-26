177d00547db93e085ad7537693103f2e96c36851
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-09-27 01:41:30 +0500

    add

---
+++ ternaryShartliOperatori/index.js
@@ -0,0 +1,8 @@
+// let points = 70;
+// let result = points > 60 ? 'O`tdi' : 'O`tmadi';
+// console.log('Imtihon natijasi: ', result);
+
+let azo = 1;
+
+let result = azo == 1 ? '20% chegirma bor sizga' : '5% chegirma bor sizga';
+console.log('Tashrifingiz uchun raxmat: ', result);
