d0b088a821b9ba88343b7085ef4a60c74bfbba73
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-09-28 18:12:51 +0500

    add

---
+++ ifElse/index.js
@@ -0,0 +1,11 @@
+let date = new Date();
+let hour = date;
+if (hour >= 6 && hour < 12){
+    console.log("Xayrli tong ");
+}
+else if (hour >=12 && hour < 18){
+    console.log("xayrli kun");
+}
+else{
+    console.log("xayrli kech");
+}
