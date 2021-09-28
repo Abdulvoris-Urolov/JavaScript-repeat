e7bb61b1359910354039ff15b21ba2b006bc41b4
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-09-28 18:32:08 +0500

    add

---
+++ switchCase/index.js
@@ -0,0 +1,22 @@
+// let word = "pencil";
+// switch(word){
+//     case "pencil":
+//         console.log("qalam");
+//         break;
+
+//     case "book":
+//         console.log("kitob");
+//         break;
+
+//     default:
+//         console.log("Noma`lum so`z");
+// }
+
+let word = "pencil";
+if(word = "pencil"){
+  
+    console.log("qalam")
+}
+else{
+    console.log("asfasfsa")
+}
