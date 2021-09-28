01005c0f91321ba5330808793b696e250a5bd2dc
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-09-28 22:26:24 +0500

    add

---
+++ doWhile/index.js
@@ -0,0 +1,14 @@
+let i = 0;
+// let a = 3;
+while(i <= 5 /*|| a <= 7*/ ) {
+    console.log(i, /*a*/);
+    i+=2;
+    // a+=2;
+}
+
+
+do{
+    console.log(i, /*a*/);
+    i+=2;
+    // a+=2; 
+} while( i <= 5 /*|| a <= 7 */) 
