738f1638ec1d02261cfc59d86d52b638cd6a5805
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-10-04 20:36:39 +0500

    add

---
+++ amaliy2/index.js
@@ -0,0 +1,20 @@
+// let a = 5;
+// let b = 8;
+
+// function name1(a, b){
+//     if(a < b){
+//         console.log(` Katta raqam ` + b);
+//     }
+//     else{
+//         console.log(' Katta raqam b ga teng edi. ');
+//     }
+// }
+
+//name1(a, b);
+
+
+function isLandscape(width, height) {
+    return (width > height);
+}
+
+console.log(isLandscape(5, 6));
